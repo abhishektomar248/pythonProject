0489ff639d9f95524d321ecd0f72d6feec818313
abhishektomar248 <abhishektomar248@gmail.com>
2024-10-12 20:39:43 +0530

    Day1

---
+++ ty.py
@@ -0,0 +1,23 @@
+print("Hello World")
+print("Hello World", "abhishek", "Tomar")
+print("Hello World", "abhishek", "Tomar", sep='-')
+print("Hello World", "abhishek", "Tomar", end='\n')
+print("Hello World", "abhishek", "Tomar", end='\t')
+print(max(23, 45, 67, 89))
+print(max(23, 45, 67, 89, 89.9))
+print(max(23, 45, 67, 89, 89.9))
+#print(max(23, 45, 67, 89, "abh"))->Gives Type error
+# print("a")-> Indentation error
+
+age= 75
+name= 'abhishek'
+isMale=True
+op=3.14
+print(type(age))
+print(type(name))
+print(type(isMale))
+print(type(op))
+
+# How to take a user Input
+name=input("Your name")
+print(name)
